fd475a686d1f9af2c2cee679da20d4ed09431b1f
patricktunez123 <rockyyves@gmail.com>
2021-01-19 15:44:25 +0200

    hoisting

--- src/app.js
+++ src/app.js
@@ -5,3 +5,18 @@ var a = 'Hello world'
 function b () {
 
 }
+
+// Hoisting // check that i have used variables and functions before declare them (thats what we call hoisting)
+
+
+// UNCOMMENT THIS CODE BELOW TO SEE THE CHANGES
+
+// b() // this is the hoisting
+// console.log(a) // this is the hoisting
+
+// var a = 'Hello world'
+
+// function b () {
+// console.log('called the function b')
+// }
+
