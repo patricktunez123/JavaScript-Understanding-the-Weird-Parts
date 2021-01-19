0b797d6b7c8e480fb156e68c8dcf56052ceb34ee
mugenga <rockyyves@gmail.com>
2021-01-19 15:09:32 +0200

    understand this and window

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Since this code, var and func are global then we can access them by this or window(for browsers)
+// ex: this.a will give us hello world
+var a = 'Hello world'
+
+function b () {
+
+}
