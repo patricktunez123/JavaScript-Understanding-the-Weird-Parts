b1383056c0c70e4754501a6befdbe2298423404f
patricktunez123 <rockyyves@gmail.com>
2021-01-19 16:58:52 +0200

    globle object

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ function b () {
 // Hoisting // check that i have used variables and functions before declare them (thats what we call hoisting)
 
 
-// UNCOMMENT THIS CODE BELOW TO SEE THE CHANGES
+//=====UNCOMMENT THIS CODE BELOW TO SEE THE CHANGES=====
 
 // b() // this is the hoisting
 // console.log(a) // this is the hoisting
@@ -20,3 +20,20 @@ function b () {
 // console.log('called the function b')
 // }
 
+
+//=====UNCOMMENT THISE LINES OF CODE TO SEE CHANGES======
+// function b () {
+//     var myVar
+//     console.log('from b:', myVar)
+// }
+
+// function a() {
+//     var myVar = 2;
+//     console.log('from a:', myVar)
+//     b()
+// }
+
+// var myVar = 1;
+
+// console.log('Outside:', myVar)
+// a();
