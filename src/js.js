db3616d4d62be4e25269b46ca7229ecc79e5dd1c
patricktunez123 <rockyyves@gmail.com>
2021-01-20 11:58:07 +0200

    add comments

--- src/js.js
+++ src/js.js
@@ -1,1 +1,7 @@
 console.log(3 < 2 < 1) // this consoles true, chech a table on this link to see why  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+// BECAUSE
+// from left to right for the < sign
+
+// 3 < 2 ==> false which gets coercion to 0
+//then false < 1 ==> true because the false is 0 
