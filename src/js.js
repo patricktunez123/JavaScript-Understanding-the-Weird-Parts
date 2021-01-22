5b2672fa4fd94638348bd0ad533a849689e6424a
patricktunez123 <rockyyves@gmail.com>
2021-01-22 10:17:16 +0200

    functions are objects

---
+++ src/js.js
@@ -0,0 +1,9 @@
+function getPerson(){
+    return {
+        name: 'Patrick TUNEZERWANE',
+        email: 'domain@company.rw'
+    }
+}
+
+console.log(getPerson().name)
+console.log(getPerson().email)
